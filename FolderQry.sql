8362072f9ddf9bae14d8d7dc72a633a199a4593e
nonix <1888777+nonix@users.noreply.github.com>
2024-10-07 14:52:52 +0200

    Update FolderQry.sql
    Add search for not like

--- FolderQry.sql
+++ FolderQry.sql
@@ -4,4 +4,5 @@ select f.name as fname, ff.name, ffu.ops from arsfol f
         and ff.name='LDD' 
     inner join arsfolfldusr ffu
         on f.fid=ffu.fid
+            and bitand(ffu.ops,256)=0
         and ff.field = ffu.field;
